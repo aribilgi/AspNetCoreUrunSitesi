367a3829501028036d8eec4ac403d6a0ed8f4057
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-05-12 21:25:21 +0300

    12-5-2022 web api crud

---
+++ WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BL;
+using Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IRepository<Brand> _repository;
+
+        public BrandsController(IRepository<Brand> repository)
+        {
+            _repository = repository;
+        }
+    }
+}
